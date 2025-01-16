a806d7f386f853ac195aba4e5bce175f74de2f39
sepehrsharafi <sepehr.sharafi123@gmail.com>
2025-01-16 20:17:27 +0330

    proper error handeling regarding database connection

--- src/core/database/postgres-service.js
+++ src/core/database/postgres-service.js
@@ -7,8 +7,11 @@ export async function query(query, variables) {
   const client = await pool.connect();
   try {
     const res = await client.query(query, variables);
-    return res;
+    return { data: res.rows, error: null }; // Return data and no error
+  } catch (error) {
+    console.error("Database query error:", error);
+    return { data: null, error: error.message }; // Return no data and an error message
   } finally {
-    client.release();
+    client.release(); // Always release the client back to the pool
   }
 }
