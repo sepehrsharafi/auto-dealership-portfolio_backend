f473b2e8721ac0977fbe5a445921731fe41f3eec
tamim <tamimyousefi023@gmail.com>
2025-01-21 01:53:29 +0330

    update route and car_id

--- src/modules/cars/controllers.js
+++ src/modules/cars/controllers.js
@@ -8,7 +8,7 @@ import {
   getSliderService,
   getAllCarsDashboardService,
 } from "../../services/cars/service.js";
-import uploadFileToS3 from "../../SDKconf/sdk.js";
+import { uploadFileToS3, deleteFilesInDirectory } from "../../SDKconf/sdk.js";
 export const getCarsByAdminIdController = async (req, res) => {
   try {
     const adminId = req.admin.id;
@@ -107,7 +107,14 @@ export const createCarController = async (req, res) => {
       return res.status(400).json({ message: "No images uploaded" });
     }
 
-    const carID = carDataObj.car_id;
+    // Generate a unique car_id
+    const brand = carDataObj.brand.toUpperCase().substring(0, 3);
+    const model = carDataObj.model.toUpperCase().substring(0, 3);
+    const trim = carDataObj.trim.toUpperCase().substring(0, 3);
+    const randomNumber = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+
+    const carID = `${brand}${model}${trim}${randomNumber}`;
+    carDataObj.car_id = carID;
 
     // Upload files to S3 and get their URLs
     const imageUrls = await Promise.all(
@@ -124,14 +131,13 @@ export const createCarController = async (req, res) => {
       })
     );
 
-    //clear failed uploads
+    // Clear failed uploads
     const validImageUrls = imageUrls.filter((url) => url !== null);
 
     if (validImageUrls.length === 0) {
       return res.status(400).json({ message: "Failed to upload any images" });
     }
 
-    // Add the image paths to the car data object
     carDataObj.image_urls = validImageUrls;
 
     const createResult = await createCarService(carDataObj);
@@ -154,33 +160,43 @@ export const createCarController = async (req, res) => {
     });
   }
 };
-export const deleteCarByIdController = async (req, res) => {
-  try {
-    const carId = req.validatedParams.car_id;
-    const deleteResult = await deleteCarByIdService(carId);
-    if (deleteResult === null) {
-      res.status(424).json({
-        message: `car with id=${carId} not deleted!!`,
-      });
-    } else {
-      res.status(201).json({
-        message: `car with id=${carId} is deleted`,
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-};
 
 export const updateCarByIdController = async (req, res) => {
   try {
-    const carId = req.validatedParams.id;
     const updateList = req.validatedBody;
+    const uploadedFiles = req.files;
+    const carId = req.validatedParams.car_id;
+    const prevImageUrls = updateList.image_urls;
+    if (!uploadedFiles || uploadedFiles.length === 0) {
+      console.log("no new images uploaded");
+    }
+
     const columns = Object.keys(updateList);
     let allUpdatesSuccessful = true;
+
+    // Upload files to S3 and get their URLs
+    const imageUrls = await Promise.all(
+      uploadedFiles.map(async (file, index) => {
+        try {
+          await deleteFilesInDirectory(carId, prevImageUrls);
+          const url = await uploadFileToS3(file, carId, index);
+          console.log(url);
+
+          return url;
+        } catch (error) {
+          console.error(`Failed to upload file: ${file.originalname}`, error);
+          return null;
+        }
+      })
+    );
+
+    const validImageUrls = imageUrls.filter((url) => url !== null);
+
+    if (validImageUrls.length === 0) {
+      return res.status(400).json({ message: "Failed to upload any images" });
+    }
+
+    updateList.image_urls = validImageUrls;
 
     for (const column of columns) {
       const updateResult = await updateCarByIdService(
@@ -205,6 +221,27 @@ export const updateCarByIdController = async (req, res) => {
         message: `Failed to update car with id=${carId}.`,
       });
       console.log(`Failed to update car with id=${carId}.`);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteCarByIdController = async (req, res) => {
+  try {
+    const carId = req.validatedParams.car_id;
+    const deleteResult = await deleteCarByIdService(carId);
+    if (deleteResult === null) {
+      res.status(424).json({
+        message: `car with id=${carId} not deleted!!`,
+      });
+    } else {
+      res.status(201).json({
+        message: `car with id=${carId} is deleted`,
+      });
     }
   } catch (error) {
     console.error(error);
--- src/modules/cars/validations.js
+++ src/modules/cars/validations.js
@@ -1,26 +1,4 @@
 import Joi from "joi";
-
-const carSchema = Joi.object({
-  admin_id: Joi.number().integer().required(), // Foreign key admin ID
-  active: Joi.boolean().default(true), // Active status (boolean, default true)
-  brand: Joi.string().required().trim().max(255), // Brand (required, max length 255)
-  model: Joi.string().required().trim().max(255), // Model (required, max length 255)
-  trim: Joi.string().trim().max(255).optional(), // Trim (optional, max length 255)
-  mileage: Joi.number().integer().min(0).optional(), // Mileage (optional, non-negative)
-  year_manufactured: Joi.number().integer().min(1886).required(), // Year manufactured (required, >= 1886)
-  body_condition: Joi.string().trim().max(255).optional(), // Body condition (optional, max length 255)
-  short_description: Joi.string().trim().optional(), // Short description (optional)
-  type: Joi.string().trim().max(255).optional(), // Type (optional, max length 255)
-  payment_type: Joi.string().optional(), // Payment type (optional, JSONB)
-  tags: Joi.array().items(Joi.string()).optional(), // Tags (optional, array of strings)
-  gearbox: Joi.string().trim().max(255).optional(), // Gearbox (optional, max length 255)
-  chassis_condition: Joi.string().trim().max(255).optional(), // Chassis condition (optional, max length 255)
-  engine_condition: Joi.string().trim().max(255).optional(), // Engine condition (optional, max length 255)
-  exterior_color: Joi.string().trim().max(255).optional(), // Exterior color (optional, max length 255)
-  interior_color: Joi.string().trim().max(255).optional(), // Interior color (optional, max length 255)
-  full_description: Joi.string().trim().optional(), // Full description (optional)
-}).required();
-
 export const getCarByIdValidaitor = async (req, res, next) => {
   console.log(req.params);
   try {
@@ -39,7 +17,28 @@ export const getCarByIdValidaitor = async (req, res, next) => {
 
 export const createCarValidator = async (req, res, next) => {
   try {
-    const validationBody = await carSchema.validateAsync(req.body);
+    const bodySchema = Joi.object({
+      admin_id: Joi.number().integer().required(), // Foreign key admin ID
+      active: Joi.boolean().default(true), // Active status (boolean, default true)
+      brand: Joi.string().required().trim().max(255), // Brand (required, max length 255)
+      model: Joi.string().required().trim().max(255), // Model (required, max length 255)
+      trim: Joi.string().trim().max(255).optional(), // Trim (optional, max length 255)
+      mileage: Joi.number().integer().min(0).optional(), // Mileage (optional, non-negative)
+      year_manufactured: Joi.number().integer().min(1886).required(), // Year manufactured (required, >= 1886)
+      body_condition: Joi.string().trim().max(255).optional(), // Body condition (optional, max length 255)
+      short_description: Joi.string().trim().optional(), // Short description (optional)
+      type: Joi.string().trim().max(255).optional(), // Type (optional, max length 255)
+      payment_type: Joi.string().optional(), // Payment type (optional, JSONB)
+      tags: Joi.array().items(Joi.string()).optional(), // Tags (optional, array of strings)
+      gearbox: Joi.string().trim().max(255).optional(), // Gearbox (optional, max length 255)
+      chassis_condition: Joi.string().trim().max(255).optional(), // Chassis condition (optional, max length 255)
+      engine_condition: Joi.string().trim().max(255).optional(), // Engine condition (optional, max length 255)
+      exterior_color: Joi.string().trim().max(255).optional(), // Exterior color (optional, max length 255)
+      interior_color: Joi.string().trim().max(255).optional(), // Interior color (optional, max length 255)
+      full_description: Joi.string().trim().optional(), // Full description (optional)
+    }).required();
+
+    const validationBody = await bodySchema.validateAsync(req.body);
     req.validatedBody = validationBody;
 
     next();
@@ -47,6 +46,49 @@ export const createCarValidator = async (req, res, next) => {
     res.status(400).json({ message: err.message });
   }
 };
+export const updateCarByIdValidator = async (req, res, next) => {
+  try {
+    const paramsSchema = Joi.object({
+      car_id: Joi.string().required(), // car ID must be a positive integer
+    });
+
+    const bodySchema = Joi.object({
+      admin_id: Joi.number().integer().required(), // Foreign key admin ID
+      active: Joi.boolean().default(true), // Active status (boolean, default true)
+      brand: Joi.string().required().trim().max(255), // Brand (required, max length 255)
+      model: Joi.string().required().trim().max(255), // Model (required, max length 255)
+      trim: Joi.string().trim().max(255).optional(), // Trim (optional, max length 255)
+      mileage: Joi.number().integer().min(0).optional(), // Mileage (optional, non-negative)
+      year_manufactured: Joi.number().integer().min(1886).required(), // Year manufactured (required, >= 1886)
+      image_urls: Joi.array().optional(),
+      body_condition: Joi.string().trim().max(255).optional(), // Body condition (optional, max length 255)
+      short_description: Joi.string().trim().optional(), // Short description (optional)
+      type: Joi.string().trim().max(255).optional(), // Type (optional, max length 255)
+      payment_type: Joi.string().optional(), // Payment type (optional, JSONB)
+      tags: Joi.array().items(Joi.string()).optional(), // Tags (optional, array of strings)
+      gearbox: Joi.string().trim().max(255).optional(), // Gearbox (optional, max length 255)
+      chassis_condition: Joi.string().trim().max(255).optional(), // Chassis condition (optional, max length 255)
+      engine_condition: Joi.string().trim().max(255).optional(), // Engine condition (optional, max length 255)
+      exterior_color: Joi.string().trim().max(255).optional(), // Exterior color (optional, max length 255)
+      interior_color: Joi.string().trim().max(255).optional(), // Interior color (optional, max length 255)
+      full_description: Joi.string().trim().optional(), // Full description (optional)
+    }).required();
+
+    // Validate the params and body
+    const validatedParams = await paramsSchema.validateAsync(req.params);
+    const validatedBody = await bodySchema.validateAsync(req.body);
+
+    // Attach the validated data to the request object
+    req.validatedParams = validatedParams;
+    req.validatedBody = validatedBody;
+
+    // Proceed to the next middleware
+    next();
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
 export const deleteCarByIdValidator = async (req, res, next) => {
   try {
     const paramsSchema = Joi.object({
@@ -55,27 +97,6 @@ export const deleteCarByIdValidator = async (req, res, next) => {
 
     const validatedParams = await paramsSchema.validateAsync(req.params);
     req.validatedParams = validatedParams;
-    next();
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-export const updateCarByIdValidator = async (req, res, next) => {
-  try {
-    const paramsSchema = Joi.object({
-      id: Joi.number().integer().positive().required(), // car ID must be a positive integer
-    });
-
-    // Validate the params and body
-    const validatedParams = await paramsSchema.validateAsync(req.params);
-    const validatedBody = await carSchema.validateAsync(req.body);
-
-    // Attach the validated data to the request object
-    req.validatedParams = validatedParams;
-    req.validatedBody = validatedBody;
-
-    // Proceed to the next middleware
     next();
   } catch (err) {
     res.status(400).json({ message: err.message });
